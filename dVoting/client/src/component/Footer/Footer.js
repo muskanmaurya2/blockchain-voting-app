b9c1933c28d0350c0d46731eb27d33154b82dd0b
Muskan <muskanmauryarly567@gmail.com>
2025-11-16 11:35:04 +0530

    decentralised-voting-system

---
+++ dVoting/client/src/component/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
